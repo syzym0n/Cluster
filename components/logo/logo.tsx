6beae3b0dc44418989e930a0053a3ad546521e32
Syzymon <syzymoncode@gmail.com>
2024-12-17 18:35:52 +0100

    Redirection '/' sur Logo

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
+
 export default function Logo() {
     return (
-      <div className="flex items-center gap-3">
+      <Link href='/'
+      className="flex items-center gap-3">
         <img src="/icons/basicIcon.png" alt="A server surrounded by magic sparkles." 
         className="w-10"/>
         <h1 className="text-xl">CLUSTER</h1>
-      </div>
+      </Link>
     );
   }
