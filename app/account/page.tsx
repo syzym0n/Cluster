f877bea121867786a820351e7c4b6e04f0f11a16
Syzymon <syzymoncode@gmail.com>
2024-12-27 16:07:52 +0100

    Intégration du DND à la page account

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,38 @@
-import Logo from "@/components/logo/logo"
+'use client';
 
-export default function Account() {
-    return (
-        <div>
-            <Logo />
-            <h1>Gestion de Compte</h1>
-        </div>
-    );
-}
+import React, { useEffect, useState } from 'react';
+import Logo from '@/components/logo/logo';
+import DndClassOrder from '@/components/accountPage/dndClassOrder/dndClassOrder';
+
+const AccountPage = () => {
+  const [userData, setUserData] = useState<any>(null); 
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/frontendDataTest.json');
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error('Erreur lors du chargement des données :', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!userData) {
+    return <p>Chargement des données...</p>;
+  }
+
+
+  return (
+    <div className="p-8">
+      <Logo />
+      <DndClassOrder initialOrder={userData[0].classOrder} />
+    </div>
+  );
+};
+
+export default AccountPage;
